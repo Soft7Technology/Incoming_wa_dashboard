4567cf86907b5d4d81d8f333825fd6f6a4469d5a
soft7 <ayushm1247@gmail.com>
2026-06-21 18:47:02 +0530

    add credit fix

--- src/routes/company.route.ts
+++ src/routes/company.route.ts
@@ -2,39 +2,40 @@ import { Router } from 'express';
 import CompanyController from '@surefy/console/http/controllers/company.controller';
 import companyController from '@surefy/console/http/controllers/company.controller';
 import { uploadMediaMiddleware } from '@surefy/middleware/upload.middleware';
+import { requireRole } from '@surefy/middleware/jwtAuth.middleware';
 
 const companyRoute = Router();
 
 // All company endpoints - JWT authentication applied at route group level
 // Only admins can create/manage companies
 
-companyRoute.post('/',uploadMediaMiddleware, CompanyController.onboard); 
-companyRoute.put('/',uploadMediaMiddleware, CompanyController.updateCompany);
+companyRoute.post('/', uploadMediaMiddleware, CompanyController.onboard);
+companyRoute.put('/:companyId', uploadMediaMiddleware, CompanyController.updateCompany);
+companyRoute.put('/', uploadMediaMiddleware, CompanyController.updateCompany);
+companyRoute.delete('/:companyId', CompanyController.deleteCompany);
 companyRoute.delete('/', CompanyController.deleteCompany);
-companyRoute.get('/dashboard', CompanyController.getdashboardStats)
-companyRoute.get('/details',companyController.getCompanyDetails)
+companyRoute.put('/:companyId/suspend', requireRole('superadmin'), companyController.suspendCompany);
+companyRoute.put('/:companyId/active', requireRole('superadmin'), companyController.activeCompany);
+companyRoute.get('/dashboard', CompanyController.getdashboardStats);
+companyRoute.get('/details', companyController.getCompanyDetails);
 
 /**
  * Get All Companies
  */
-companyRoute.get('/', CompanyController.getAllCompanies );
-
+companyRoute.get('/', CompanyController.getAllCompanies);
 
 /**
  * Companies user route
  */
-companyRoute.post('/user',companyController.createUser);
-companyRoute.get('/user',companyController.getAllUsers);
+companyRoute.post('/user', companyController.createUser);
+companyRoute.get('/user', companyController.getAllUsers);
 companyRoute.put('/user/:id', CompanyController.updateCompanyUser);
 
-
-companyRoute.get("/subscriptions",CompanyController.getCompaniesSubscription)
-
+companyRoute.get('/subscriptions', CompanyController.getCompaniesSubscription);
+companyRoute.get('/stats', CompanyController.getUserStats);
+companyRoute.get('/user/plan/:userId', companyController.getUserPlan);
+companyRoute.get('/user-details/:userId', companyController.getUserDetails);
 companyRoute.get('/:id', CompanyController.getById);
-companyRoute.get('/user-details/:userId',companyController.getUserDetails)
-
 companyRoute.post('/:planId/regenerate-keys', CompanyController.regenerateKeys);
-companyRoute.get('/stats', CompanyController.getUserStats)
-companyRoute.get('/user/plan/:userId', companyController.getUserPlan)
 
 export default companyRoute;
