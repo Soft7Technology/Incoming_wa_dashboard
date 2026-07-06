edaec40ec57bff6f4500db77f25ffb4b533a4d69
Soft7Technology <aakanksha@soft7.in>
2026-07-06 12:28:46 +0530

    Done

--- src/app/models/message.model.ts
+++ src/app/models/message.model.ts
@@ -430,7 +430,6 @@ class MessageModel extends BaseModel {
   // }
 
 
-
   async getLeadConversations(
     contactNumber: string,
     phone_number_id: string,
@@ -507,7 +506,8 @@ class MessageModel extends BaseModel {
       const messageTime = new Date(msg.created_at).getTime();
 
       return (
-        msg.direction === 'inbound' &&
+        msg.direction === 'inbound' ||
+        msg.type === 'template' &&
         now - messageTime <= TWENTY_FOUR_HOURS
       );
     });
