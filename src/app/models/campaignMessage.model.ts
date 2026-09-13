5cddc1b4aa869c2ccfd0c9828741c9702e8d13bf
parthvichare <parthuvichare9@gmail.com>
2026-09-13 12:07:54 +0530

    Done

--- src/app/models/campaignMessage.model.ts
+++ src/app/models/campaignMessage.model.ts
@@ -217,7 +217,6 @@ class CampaignMessageModel extends BaseModel {
         this.db.raw(`COUNT(*) FILTER (WHERE cm.status = 'pending')  AS pending_count`),
         this.db.raw(`COUNT(*) FILTER (WHERE m.status = 'delivered') AS delivered_count`),
         this.db.raw(`COUNT(*) FILTER (WHERE m.status = 'read')    AS read_count`),
-        this.db.raw(`COUNT(*) FILTER (WHERE m.status = 'failed' OR cm.status = 'failed')  AS failed_count`),
         this.db.raw(`
            COUNT(*) FILTER(
              WHERE cm.status = 'failed' OR m.status = 'failed'
